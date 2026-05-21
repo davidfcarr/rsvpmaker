9ebd68d0ee1a821ac1ef78538a1326b244b6cd95
David F. Carr <david@carrcommunications.com>
2026-05-21 11:59:31 -0400

    Fix condition to check RSVP meta value

--- admin/src/rsvpbutton/render.php
+++ admin/src/rsvpbutton/render.php
@@ -1,7 +1,7 @@
 <div <?php echo get_block_wrapper_attributes(); ?>>
 <?php
 global $post;
-if(get_post_meta($post->ID,'_rsvp_on',true)) {
+if(!empty(get_post_meta($post->ID,'_rsvp_on',true))) {
     echo get_rsvp_link_custom($post->ID,$content);
 }
 ?>
